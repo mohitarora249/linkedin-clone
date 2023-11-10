687e8a81e09d9e2c0fa8d562cbc1be79b615db4f
mohitarora249 <mohitarora249@gmail.com>
2023-11-10 15:10:54 +0000

    added antd lib and CreatePostCard init

--- app/feed/(ui)/CreatePostCard.tsx
+++ app/feed/(ui)/CreatePostCard.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Card, Avatar, Input, Button, Flex } from 'antd';
+import { DownloadOutlined, UserOutlined } from '@ant-design/icons';
 
 const CreatePostCard = () => {
-    return <div className="card">CreatePostCard</div>
+    return (
+        <Card className="">
+            <div className="flex space-x-4">
+                <Avatar shape="circle" size={36} icon={<UserOutlined />} />
+                <Input size="large" placeholder="Start a post" />
+            </div>
+            <ActionButtons />
+        </Card>
+    )
+}
+
+const ActionButtons = () => {
+    return (
+        <Flex gap="small" wrap="wrap" className="mt-2" justify="space-around">
+            <Button type="text" size="large" icon={<DownloadOutlined />}>Media</Button>
+            <Button type="text" size="large" icon={<DownloadOutlined />}>Event</Button>
+            <Button type="text" size="large" icon={<DownloadOutlined />}>Write Article</Button>
+        </Flex>
+    )
 }
 
 export default CreatePostCard;
