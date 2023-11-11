bbb4b7ef16041bca95e6c2f4aad01369db043e0a
mohitarora249 <mohitarora249@gmail.com>
2023-11-11 04:31:38 +0000

    Navbar component done

--- app/(ui)/Navbar.tsx
+++ app/(ui)/Navbar.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Layout, Menu } from "antd";
+const { Header } = Layout;
+
 
 const Navbar = () => {
-    return <div>Navbar</div>
+    return (
+        <Header>
+            <Menu
+                // theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={['2']}
+                items={new Array(5).fill(null).map((_, index) => {
+                    const key = index + 1;
+                    return {
+                        key,
+                        label: `nav ${key}`,
+                    };
+                })}
+            />
+        </Header>
+    )
 }
 
 export default Navbar;
---
+++ app/notifications/layout.tsx
@@ -0,0 +1,24 @@
+"use client"
+import React from "react";
+import type { Metadata } from 'next'
+import { Layout, Menu } from "antd";
+import Navbar from "../(ui)/Navbar";
+const { Header, Footer, Sider, Content } = Layout;
+
+// export const metadata: Metadata = {
+//   title: 'Feed | LinkedIn Clone'
+// }
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <Layout className="max-w-6xl mx-auto">
+      <Navbar />
+      <Content className="m-5">{children}</Content>
+    </Layout>
+    
+  )
+}
